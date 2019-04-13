e4f0cacc1af7879fdcce59a28f0554def6f763b6
maddybvh <maddybvh@gmail.com>
2019-04-13 13:38:14 -0400

    hover function working

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const container = document.querySelector('#container');
 
+function changeColor () {
+    this.style.backgroundColor = 'black';
+    console.log('mouseover!')
+}
+
+//add squares to the container
 for (i = 0; i < 256; i++) {
     const square = document.createElement('div');
     square.classList.add('square');
+    square.addEventListener('mouseover', changeColor);
     container.appendChild(square);
-
 }
