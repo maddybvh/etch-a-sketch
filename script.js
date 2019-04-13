568e77290333e036db88d572d9d10f341a53fa0c
maddybvh <maddybvh@gmail.com>
2019-04-13 12:26:57 -0400

    hover state, one column of squares

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('#container');
+
+for (i = 0; i < 16; i++) {
+    const square = document.createElement('div');
+    square.classList.add('square');
+    container.appendChild(square);
+
+}
