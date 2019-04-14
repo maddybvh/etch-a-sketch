ddac3e9b6651d47531c6e4b595dff0184f899b68
maddybvh <maddybvh@gmail.com>
2019-04-13 21:56:27 -0400

    Styling

--- script.js
+++ script.js
@@ -1,29 +1,7 @@
-function changeColor(){
-    this.style.backgroundColor = chooseColor();
-}
 
-function chooseColor(){
-    switch(colorScheme){
-        case 'black':
-            return 'black';
-            break;
-        case 'rainbow':
-            return makeRandomColor();
-    }
-}
-
-function makeRandomColor() {
-    return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
-}
-
-
-
-//Make the grid by adding squares to the container
 function makeGrid (squaresPerSide){
-    //create new grid
     let totalSquares = squaresPerSide * squaresPerSide;
     let sideLength = (600 / squaresPerSide) + 'px';
-    console.log(sideLength)
     for (i = 0; (i < totalSquares); i++) {
         const container = document.querySelector('#container')
         const square = document.createElement('div');
@@ -35,33 +13,52 @@ function makeGrid (squaresPerSide){
     }
 }
 
+function changeColor(){
+    this.style.backgroundColor = chooseColor();
+}
 
-//Buttons and actions
+function chooseColor(){
+    switch(colorScheme){
+        case 'black':
+            return 'black';
+            break;
+        case 'rainbow':
+            return 'hsla(' + (Math.random() * 360) + ', 100%, 50%, 1)';
+    }
+}
 
-document.getElementById("a").addEventListener("click", function(){
-    //remove old squares
+function removeOldSquares(){
     oldSquares = document.getElementsByClassName('square');
     while (oldSquares[0]){
         oldSquares[0].remove();
     }
-    
-    squaresPerSide = prompt('How many squares per side?', '');
-    makeGrid(squaresPerSide);
-    });
+}
 
-document.getElementById("b").addEventListener("click", function(){
+//Buttons
+document.getElementById("btnStart").addEventListener("click", function(){
+    squaresPerSide = prompt('How many squares per side?', '');
+    removeOldSquares();
+    makeGrid(squaresPerSide);
+});
+
+document.getElementById("btnPen").addEventListener("click", function(){
     switch (colorScheme){
         case 'black':
             colorScheme = 'rainbow'
-            document.getElementById("b").innerHTML = 'Change to black';
+            document.getElementById("btnPen").innerHTML = 'Black pen';
+            document.getElementById("btnPen").style.backgroundImage = 'url(https://bit.ly/2UGeRFe)'
+            document.getElementById("btnPen").style.color = 'white';
             break;
         case 'rainbow':
             colorScheme = 'black';
-            document.getElementById("b").innerHTML = 'Change to rainbow';
+            document.getElementById("btnPen").innerHTML = 'Rainbow pen';
+            document.getElementById("btnPen").style.backgroundImage = 'url(https://bit.ly/2UiPO5U)'
+            document.getElementById("btnPen").style.color = 'black';
     }
     changeColor;
 });
 
+//runs at start
 let squaresPerSide = 16;
 let colorScheme = 'black';
 makeGrid(squaresPerSide);
